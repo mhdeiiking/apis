1a7dade8ac7b896bad8559e4dab52a012c9102d1
Mahdi Diaa <69827418+mhdeiiking@users.noreply.github.com>
2023-03-19 17:37:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
-import pyrogram , pyromod
-
-from pyromod import listen
-from .keep import alive
-from pyrogram import Client, filters, enums
-p = dict(root='plugins')
-x=Client(name='loclhost', api_id=15102119, api_hash='3dfdcee3e3bedad4738f81287268180f', bot_token='6172993508:AAHCBVM-QeFEb9VoHj8Kygd71TWCuoyIKwg', workers=20, plugins=p, parse_mode=enums.ParseMode.DEFAULT)
-alive()
-x.run()
+import requests,time
+from time import sleep
+while True:
+  requests.get('https://y.itstrakos.repl.co')
+  sleep(5)
